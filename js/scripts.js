ffc370a8a254639ab8c28496c819589cfd7841b0
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2021-10-20 13:57:22 -0700

    finished conversions...
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,17 @@
+function galToLit(gal) {
+  return gal / 0.26;
+}
+
 $(document).ready(function() {
   $("form#appointment").submit(function(event) {
     event.preventDefault();
     $("#output").text("Submitted!")
+  })
+
+  $("form#conversion").submit(function(event) {
+    event.preventDefault();
+    const result = parseFloat($("#gallons").val());
+    const ans = galToLit(result);
+    $("#output2").text(ans);
   })
 })
